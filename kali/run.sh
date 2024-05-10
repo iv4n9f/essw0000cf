296bb2b531da4ffd26f3722077063b717c40d492
Ivan <iv4n9f@gmail.com>
2024-05-10 15:00:44 +0200

    0.002

--- kali/run.sh
+++ kali/run.sh
@@ -4,12 +4,14 @@ dir=$1
 shell=$(echo $SHELL | rev | cut -d "/" -f 1 | rev)
 user=$(whoami)
 
-packages="bspwm sxhkd polybar feh rofi jq gnome-terminal xclip dconf-cli bat nitrogen libpango1.0-dev"
+dos2unix $dir/../shared/bspwm/scripts/*
+packages="bspwm sxhkd polybar feh rofi jq gnome-terminal xclip dconf-cli bat nitrogen libpango1.0-dev compton"
 sudo apt-get install $packages -y
-cp -r $dir/../shared/bspwm $HOME/.config/ && cp -r $dir/../shared/sxhkd $HOME/.config/ && cp -r $dir/../shared/polybar $HOME/.config/ && cp /etc/X11/xinit/xinitrc $HOME/.xinitrc && echo "exec bspwm" > $HOME/.xinitrc && chmod +x $HOME/.xinitrc
+cp -r $dir/../shared/bspwm $HOME/.config/ && chmod +x $HOME/.config/bspwm/scripts/* && cp -r $dir/../shared/sxhkd $HOME/.config/ && cp -r $dir/../shared/polybar $HOME/.config/ && cp /etc/X11/xinit/xinitrc $HOME/.xinitrc && echo "exec bspwm" > $HOME/.xinitrc && chmod +x $HOME/.xinitrc && cp -r $dir/../shared/compton $HOME/.config/
 mkdir -p $HOME/Downloads && cd $HOME/Downloads && wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip" && unzip Hack.zip &&  sudo mkdir -p /usr/share/fonts/Hack && sudo cp *.ttf /usr/share/fonts/Hack/. && cd $dir
 cat $dir/../shared/utils/alias >> $HOME/.$(echo $shell)rc
-dos2unix $dir/../shared/utils/bin/* | sudo chown $user:$user $dir/../shared/utils/bin/* && chmod 775 $dir/../shared/utils/bin/* && sudo cp $dir/../shared/utils/bin/* /usr/bin/.
-mkdir -p $HOME/Pictures && cp $dir/../shared/resources/wallpaper.png $HOME/Pictures/.
-$dir/../shared/utils/gnome-terminal.sh $dir/../shared/utils/gnome.conf
+dos2unix $dir/../shared/utils/bin/* && sudo cp $dir/../shared/utils/bin/* /usr/bin/.
 
+sudo chmod 775 /usr/bin/background && sudo chmod 775 /usr/bin/set_target
+/usr/bin/set_target 8.8.8.8
+$dir/../shared/utils/gnome-terminal.sh $dir/../shared/utils/gnome.conf
