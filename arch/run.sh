25b9952958ce1db6ff335fcafdb8a07e6c8502d6
iv4n9f <iv4n9f@gmail.com>
2024-07-23 11:37:26 +0200

    0.004

--- arch/run.sh
+++ arch/run.sh
@@ -21,8 +21,8 @@ fi
 dos2unix $dir/../shared/bspwm/scripts/*
 
 # List of packages to install on Arch Linux
-packages="bspwm sxhkd polybar feh rofi jq gnome-terminal xclip dconf bat nitrogen pango compton"
-yay -S --noconfirm $packages
+packages="bspwm sxhkd polybar feh rofi jq gnome-terminal xclip dconf bat nitrogen pango compton x-server lightdm-gtk-greeter"
+pacman -S --noconfirm $packages
 
 # Copy and set permissions for configuration files
 cp -r $dir/../shared/bspwm $HOME/.config/
